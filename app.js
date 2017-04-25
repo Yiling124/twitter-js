00d3d8af6fe69fca7de9b9f6ddf0b316e74fdf5b
Yiling Jiang <yiling124@gmail.com>
2017-04-25 13:03:51 -0400

    update2

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
+var nunjucks = require("nunjucks");
 var express = require("express");
+var tweetBank = require('./tweetBank');
+var routes = require("./routes");
+//var router = require("index")
 var app = express();  // creates an instance of an express application
+
+
+app.use("/", routes);
+
+app.set("view engine", "html");
+app.engine("html", nunjucks.render);
+nunjucks.configure("views", {nocache: true});
+
+
+// var locals = {
+// 	title: "An example",
+// 	people: [
+// 		{ name: 'Gandalf'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'}
+// 	]
+// }
+
+// nunjucks.render("index.html", locals, function(err, output){
+// 	console.log(output);
+// })
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+
+app.get('/', function (req, res, next) {
+  	res.render("index",{title: "HALL OF FAME",people: people}, function(err, html){
+		res.send(html);
+	})
+})
+
+// res.render("index",{title: "HALL OF FAME",people: people}, function(err, html){
+// 	res.send(html);
+// })
+
+
+// static routing 
+app.get("/", function(req, res, next){
+	res.sendFile("/public/stylesheets/style.css")
+})
+
 
 app.use(function(req, res, next){
 	console.log(req.method + req.originalUrl);
@@ -15,15 +59,18 @@ app.get('/news', function (req, res, next) {
 })
 
 
-app.use('/special/',function(req, res, next){
+// app.use('/special/',function(req, res, next){
 
-	console.log(req.originalUrl.split("/"));
-	if (req.originalUrl.split("/").includes("special")){
-		console.log("you reached the special area.")
-	}
-})
+// 	console.log(req.originalUrl.split("/"));
+// 	if (req.originalUrl.split("/").includes("special")){
+// 		console.log("you reached the special area.")
+// 	}
+// })
 
 
 app.listen(3000, function () {
   console.log('Server is listening')
 })
+
+
+
