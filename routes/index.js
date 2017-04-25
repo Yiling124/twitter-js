7ceb3653a3a4846b291bea805d53c70f35fd4c37
Yiling Jiang <yiling124@gmail.com>
2017-04-25 15:45:41 -0400

    v4

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,17 @@ const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 
+
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
+});
+
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  res.render( 'index', {tweets: list} );
 });
 
 //router.get()
